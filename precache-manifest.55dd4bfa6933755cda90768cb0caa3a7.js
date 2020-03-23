27c0ac562f4be9de39fc7fde4893bbff925b1cc0
IZUNA894 <48329138+IZUNA894@users.noreply.github.com>
2020-03-23 17:53:56 +0530

    Updates

---
+++ precache-manifest.55dd4bfa6933755cda90768cb0caa3a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115b010830efb269f341058dd61901f5",
+    "url": "/Ninja-way/index.html"
+  },
+  {
+    "revision": "7e7f1245ab4061f54c84",
+    "url": "/Ninja-way/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "15552d7bba3ef2858800",
+    "url": "/Ninja-way/static/js/2.6c935757.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Ninja-way/static/js/2.6c935757.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e7f1245ab4061f54c84",
+    "url": "/Ninja-way/static/js/main.174780d3.chunk.js"
+  },
+  {
+    "revision": "a0baa5d08263cfbb2c2e",
+    "url": "/Ninja-way/static/js/runtime-main.b8828a15.js"
+  }
+]);
